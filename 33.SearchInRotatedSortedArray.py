a4099e5b44529498b6b5b1e96f87d5b4b0aafad2
RoxyYujinKim <yujinkim0707@gmail.com>
2022-05-27 14:54:33 +0900

    feat: solve 33.SearchInRotatedSortedArray

---
+++ 33.SearchInRotatedSortedArray.py
@@ -0,0 +1,32 @@
+import bisect
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 1:
+            if nums[0] == target:
+                return 0
+            return -1
+        
+        left, right = 0, len(nums)-1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            
+            if nums[left] == target:
+                return left
+            elif nums[right] == target:
+                return right
+            elif nums[mid] == target:
+                return mid
+            elif nums[left] < nums[mid]:
+                if nums[left] < target and target < nums[mid]:
+                    right = mid-1
+                else: 
+                    left = mid+1
+            else:
+                if nums[mid] < target and target <= nums[right]:
+                    left = mid+1
+                else:
+                    right = mid-1
+                    
+        return -1
