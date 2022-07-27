06462bfca84869c39b60d2822c128fce95e3520c
RoxyYujinKim <yujinkim0707@gmail.com>
2022-07-28 06:54:49 +0900

    solve: solve beakjoon 2407.조합

---
+++ beakjoon/2407.조합.py
@@ -0,0 +1,14 @@
+import sys
+
+def dp(n, m):
+    dp = [[1] * (m+1) for _ in range(n+1)]
+
+    for i in range(2, n+1):
+        for j in range(1, m+1):
+            if i == j: dp[i][j] = 1
+            else: dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+
+    return dp[-1][-1]
+
+n, m = map(int, sys.stdin.readline().split())
+print(dp(n, m))
