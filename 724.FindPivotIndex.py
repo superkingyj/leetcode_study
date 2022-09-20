afce8ec18e4bab74aab633e04ccfdad651741373
RoxyYujinKim <yujinkim0707@gmail.com>
2022-09-20 14:48:48 +0900

    update: upate 724.FindPivotIndex

--- 724.FindPivotIndex.py
+++ 724.FindPivotIndex.py
@@ -1,3 +1,5 @@
+from typing import *
+
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         n = len(nums)
@@ -20,5 +22,16 @@ class Solution:
                 break
         
         return result
-                
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left, right = 0, sum(nums)
+        
+        for idx, num in enumerate(nums):
+            right -= num
+            if left == right:
+                return idx
+            left += num
+            
+        return -1          
         
