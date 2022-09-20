3d54c35bb36ff38461f67b143655500102432a6c
RoxyYujinKim <yujinkim0707@gmail.com>
2022-09-20 14:43:16 +0900

    solve: solve 724.FindPivotIndex

---
+++ 724.FindPivotIndex.py
@@ -0,0 +1,24 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        n = len(nums)
+        left = [0] * (n+1)
+        right = [0] * (n+1)
+        result = -1
+        
+        left[1] = nums[0]
+        right[n-1] = nums[-1]
+        
+        for i in range(2,n+1):
+            left[i] = left[i-1] + nums[i-1]
+        
+        for i in range(n-2, -1, -1):
+            right[i] = right[i+1] + nums[i]
+        
+        for i in range(n):
+            if left[i] == right[i+1]:
+                result = i
+                break
+        
+        return result
+                
+        
