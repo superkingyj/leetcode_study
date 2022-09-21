99c30fafb1688cf34a3fcb3e0941840166094ce0
RoxyYujinKim <yujinkim0707@gmail.com>
2022-09-21 14:20:10 +0900

    update: update 1248.파티

--- beakjoon/1248.파티.py
+++ beakjoon/1248.파티.py
@@ -1,15 +1,51 @@
+# import sys
+# import heapq
+# from collections import defaultdict
+
+# N, M, X = map(int, sys.stdin.readline().split())
+# graph = defaultdict(list)
+
+# for _ in range(M):
+#     n, m, x = map(int, sys.stdin.readline().split())
+#     graph[n].append((m, x))
+
+# def dijkstra(start):
+#     q = [(0, start)] 
+#     dist = defaultdict(int)
+
+#     while q:
+#         time, node = heapq.heappop(q)
+#         if node not in dist:
+#             dist[node] = time
+#             for m, x in graph[node]:
+#                 alt = time + x
+#                 heapq.heappush(q, (alt, m))
+    
+#     return dist
+
+# result = 0
+# back = dijkstra(X)
+
+# for i in range(1, N+1):
+#     go = dijkstra(i)
+#     result = max(result, go[X] + back[i])
+
+# print(result)
+
 import sys
 import heapq
 from collections import defaultdict
 
 N, M, X = map(int, sys.stdin.readline().split())
-graph = defaultdict(list)
+graph_back = defaultdict(list)
+graph_go = defaultdict(list)
 
 for _ in range(M):
     n, m, x = map(int, sys.stdin.readline().split())
-    graph[n].append((m, x))
+    graph_back[n].append((m, x))
+    graph_go[m].append((n, x))
 
-def dijkstra(start):
+def dijkstra(start, graph):
     q = [(0, start)] 
     dist = defaultdict(int)
 
@@ -23,11 +59,11 @@ def dijkstra(start):
     
     return dist
 
+back = dijkstra(X, graph_back)
+go = dijkstra(X, graph_go)
 result = 0
-back = dijkstra(X)
 
 for i in range(1, N+1):
-    go = dijkstra(i)
-    result = max(result, go[X] + back[i])
-
+    result = max(result, go[i]+back[i])
+    
 print(result)
