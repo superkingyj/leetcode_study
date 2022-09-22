068721c6dae083e8d77b6da0ee84c91599be13e2
RoxyYujinKim <yujinkim0707@gmail.com>
2022-09-22 16:42:09 +0900

    update: update 213.HouseRobber2

--- 213.HouseRobber_2.py
+++ 213.HouseRobber_2.py
@@ -22,3 +22,20 @@ class Solution:
         
         return max(dp_st[n-1], dp_en[n-1])
 
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp0 = [0] * len(nums)
+        dp1 = [0] * len(nums)
+        
+        if len(nums) <= 1:
+            return nums[0]
+        
+        dp0[1] = nums[0]
+        for i in range(2, len(nums)):
+            dp0[i] = max(dp0[i-1], dp0[i-2]+nums[i-1])
+                
+        dp1[1] = nums[1]
+        for i in range(2, len(nums)):
+            dp1[i] = max(dp1[i-1], dp1[i-2]+nums[i])
+            
+        return max(dp0[-1], dp1[-1])
