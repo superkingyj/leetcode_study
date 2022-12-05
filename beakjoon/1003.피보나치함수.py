036c063f039a611c117cfe8c6b1d20ce5dec476b
RoxyYujinKim <yujinkim0707@gmail.com>
2022-12-05 18:27:31 +0900

    solve: solve beakjoon 1003.피보나치함수

---
+++ beakjoon/1003.피보나치함수.py
@@ -0,0 +1,21 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+dp_0 = [0] * 41
+dp_0[0] = 1
+dp_0[1] = 0
+
+for i in range(2, 41):
+    dp_0[i] = dp_0[i-1] + dp_0[i-2]
+
+dp_1 = [0] * 41
+dp_1[0] = 0
+dp_1[1] = 1
+
+for i in range(2, 41):
+    dp_1[i] = dp_1[i-1] + dp_1[i-2]
+
+for _ in range(T):
+    N = int(sys.stdin.readline())
+    print(dp_0[N], dp_1[N])
