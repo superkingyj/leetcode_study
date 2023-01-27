61ce96749dce8c4106e497fc8050d77e22751e96
RoxyYujinKim <yujinkim0707@gmail.com>
2023-01-27 16:19:05 +0900

    solve: solve beakjoon 21939.문제추천시스템version1

--- beakjoon/python/21939.문제추천시스템version1.py
+++ beakjoon/python/21939.문제추천시스템version1.py
@@ -2,53 +2,36 @@ import sys
 import heapq
 
 N = int(sys.stdin.readline())
-# 어려운 문제가 max_heap[0]
-max_heap = []
-# 쉬운 문제가 min_heap[0]
-min_heap = []
+max_heap, min_heap = [], []
 check = dict()
+
 for _ in range(N): 
     num, level = map(int, sys.stdin.readline().split())
-    heapq.heappush(min_heap, (level, num))
-    heapq.heappush(max_heap, (-level, num))
     check[num] = False
-
-result = ""
+    heapq.heappush(min_heap, (level, num))
+    heapq.heappush(max_heap, (-level, -num))
 
 M = int(sys.stdin.readline())
 for _ in range(M):
     query, *nums = sys.stdin.readline().split()
-
     if query == "add":
-        heapq.heappush(min_heap, (int(nums[1]), int(nums[0])))
-        heapq.heappush(max_heap, (-int(nums[1]), int(nums[0])))
+        while check[-max_heap[0][1]]: heapq.heappop(max_heap)
+        while check[min_heap[0][1]]: heapq.heappop(min_heap)    
         check[int(nums[0])] = False
-    
+        heapq.heappush(max_heap, (-int(nums[1]), -int(nums[0])))
+        heapq.heappush(min_heap, (int(nums[1]), int(nums[0])))
+
     # 가장 어려운 문제 출력
     elif query == "recommend" and int(nums[0]) == 1: 
-        tmp = []
-        while max_heap:
-            level, num = heapq.heappop(max_heap)
-            if not check[num]: 
-                check[num] = True
-                break
-            else: tmp.append((level, num))
-        print(-num)
-        for level, num in tmp:
-            heapq.heappush(max_heap, (level, num))
+        while check[-max_heap[0][1]]:
+            heapq.heappop(max_heap)
+        print(-max_heap[0][1])
     
     # 가장 쉬운 문제 출력
     elif query == "recommend" and int(nums[0]) == -1:
-        tmp = []
-        while min_heap:
-            level, num = heapq.heappop(min_heap)
-            if not check[num]: 
-                check[num] = True
-                break
-            else: tmp.append((level, num))
-        print(num)
-        for level, num in tmp:
-            heapq.heappush(min_heap, (level, num))
+        while check[min_heap[0][1]]:
+            heapq.heappop(min_heap)
+        print(min_heap[0][1])
     
     # 문제 제거
     else:
